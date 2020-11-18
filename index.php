ca593afa227d948c025444d79ee30577bd797db9
alexandre leroux <alexandre.leroux.photographies@gmail.com>
2020-11-18 12:27:52 +0100

    debut page ok

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="CSS/style.css" />
+    <title>welcome</title>
+</head>
+
+
+<body>
+
+
+
+<header class="container-sm-fluid sticky-top" id=header>
+
+    <nav class="h-100 navbar navbar-expand-sm navbar-dark bg-dark ">
+
+        <a class="navbar-brand h-100" href=""><img src="media/logo.png"  class="h-100 img-fluid"   alt="logo">Welcome !</a>
+
+        <button class=" navbar-toggler navbar-toggler-dark bg-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                <ul class="bg-dark justify-content-around navbar-nav ">
+
+                            <li class="nav-item ml-5 ">
+                                <a class="nav-link " href="#">Créer un compte</a>
+                            </li>
+
+                            <li class="nav-item ml-5 ">
+                                <a class="nav-link " href="#">Se connecter</a>
+                            </li>
+            
+                </ul>
+    
+        </div>
+
+    </nav>
+
+</header>
+
+<section class="prestentation">
+
+        <div class="container h-100">
+
+            <div class="row  h-100  flex-column  ">
+                
+                <h1 class="text-center my-5 text-secondary">Bienvenue chez Welcome !</h1>
+
+                <div class="row bg-light no-gutters  h-75" id=logo_presentation>
+
+                    <div class="col-4  h-75 d-flex flex-column justify-content-between">
+                        <h2 class=text-center>Les meilleures solutions</h2>
+                        <img class="img-fluid d-block mx-auto" src="media/012-mortgage loan.png" alt="">
+                    </div>
+
+                    <div class="col-4  h-75 d-flex flex-column justify-content-between">
+                        <h2 class="text-center">Des solutions adaptées à vos besoins</h2>
+                        <img class="img-fluid d-block mx-auto" src="media/013-park.png" alt="">
+                    </div>
+
+                    <div class="col-4  h-75 d-flex flex-column justify-content-between">
+                        <h2 class="text-center">Une écoute au quotidien</h2>
+                        <img class="img-fluid d-block mx-auto " src="media/014-rent.png" alt="">
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ inscription.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="accueil.css" />
+    <title>WORK IN PROGRESS</title>
+</head>
+
+
+<body>
+
+
+</body>
+
+</html>
