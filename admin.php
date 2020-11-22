5ec568ebb0d85ae48b7c527adba85addef71aeab
prénom nom <alexandre.leroux@laplateforme.io>
2020-11-22 01:31:22 +0100

    base page admin ok

--- admin.php
+++ admin.php
@@ -1,15 +1,103 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-
-    <link rel="stylesheet" href="accueil.css" />
-    <title>WORK IN PROGRESS</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="CSS/style.css" />
+    <title>profil</title>
 </head>
+
 
 
 <body>
 
+<main>
+
+
+<?php
+
+    $bdd = new PDO('mysql:host=localhost;dbname=moduleconnexion;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+?>
+
+
+
+<?php include("code/header-connect.php"); ?>
+
+<?php //récupère les données du compte pour les afficher dans les inputs
+
+
+     $req = $bdd->query('SELECT * FROM utilisateurs ');
+
+     $donnees = $req->fetch(PDO::FETCH_ASSOC);
+
+     
+?>
+        <table>
+        <thead>
+        <tr>
+            <?php             
+                    foreach ( $donnees as $key => $value)
+                    
+                    {
+                        echo '<th>'.$key.'</th> '; 
+
+                    }
+            ?>
+        </tr>
+    </thead>
+
+
+
+    <tbody>
+            <?php            
+                    echo '<tr>';
+                    foreach ( $donnees as $key => $value)
+                    
+                    {
+                        echo '<td>' .$value.'</td>'; 
+
+                    }
+                    echo '<tr/>';
+
+
+
+                    while (  (  $donnees = $req->fetch(PDO::FETCH_ASSOC))  != NULL)
+
+                    {
+
+                        echo '<tr>';  
+                        
+                        foreach ( $donnees as $key => $value){
+                            echo '<td>' .$value.'</td>'; 
+                                }  
+                        
+                        '<tr/>';
+                    }
+
+            ?>
+    </tbody>
+
+
+</table>
+
+     
+                    
+
+
+
+
+
+
+
+<?php include("code/footer.php");?>
+
+
+</main>
 
 </body>
 
