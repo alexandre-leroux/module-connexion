11aa6205a69cc886bf31660415c50e2ee5c6d331
prénom nom <alexandre.leroux@laplateforme.io>
2020-11-23 21:30:54 +0100

    maj bdd

--- sql/moduleconnexion.sql
+++ sql/moduleconnexion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : jeu. 19 nov. 2020 à 09:07
+-- Généré le : lun. 23 nov. 2020 à 20:30
 -- Version du serveur :  5.7.24
 -- Version de PHP : 7.4.1
 
@@ -41,8 +41,9 @@ CREATE TABLE `utilisateurs` (
 --
 
 INSERT INTO `utilisateurs` (`id`, `login`, `prenom`, `nom`, `password`) VALUES
-(1, 'admin', 'admin', 'admin', 'admin'),
-(2, 'Alex', 'Alexandre', 'Leroux', '1234');
+(56, 'scorfel', 'alexandre', 'Leroux', '$2y$10$NyidYM2S8CTG6xWel42HU.msUvTwJpXVIbAzkG/mtZGYvr/ZqxaAS'),
+(57, 'admin', 'admin', 'admin', '$2y$10$Ai0bAunJiRkt6csAKz0UMezZP7ZgkZiW8uR74wK354TIp3to237sG'),
+(58, 'boris', 'francois', 'becker', '$2y$10$J2uPfYoASl70Ce6bVZxHS.z.JExHll1oUzNtezKZ2fHEcGsEyoZJG');
 
 --
 -- Index pour les tables déchargées
@@ -62,7 +63,7 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
